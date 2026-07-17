12e7f8301c9c2cae09475c4653cdd171ee06ebd8
Zeferino <e.f.s.zeferino@utwente.nl>
2026-07-17 21:21:56 +0200

    Fix main imports

--- backend/api_router.py
+++ backend/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from routes.equipment_routes import router as equipment_router
+from routes.product_configuration_routes import router as product_router
 from routes.production_routes import router as production_router
 from routes.simulation_routes import router as simulation_router
 from routes.debug_routes import router as debug_router
@@ -9,6 +10,7 @@ from routes.product_material_routes import router as product_material_router
 api_router = APIRouter()
 
 api_router.include_router(equipment_router)
+api_router.include_router(product_router)
 api_router.include_router(production_router)
 api_router.include_router(simulation_router)
 api_router.include_router(debug_router)
