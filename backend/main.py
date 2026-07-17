8c3e9d61e806361ba347e700a3eee74abab76fbf
Zeferino <e.f.s.zeferino@utwente.nl>
2026-07-17 21:01:26 +0200

    Fix main imports

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api_router import api_router
 
 app = FastAPI()
 
-app.include_router(api_router)
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],   # or your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(api_router)
 
 
 
